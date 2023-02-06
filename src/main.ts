4ed3e4fad8f0e308c1eb1cde85185e7d9a3eda7a
Robin <one.robin@outlook.fr>
2023-02-06 11:50:41 +0100

    Conf fbase

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,18 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+import {firebaseConfig} from "@/fbaseconf";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import  {getAnalytics} from "firebase/analytics";
+import {getAuth} from "firebase/auth"
+
+const fbaseapp = initializeApp(firebaseConfig);
+export const analytics = getAnalytics();
+export const auth = getAuth();
+export const db = getFirestore();
+
+
 const app = createApp(App)
   .use(IonicVue)
   .use(router);
